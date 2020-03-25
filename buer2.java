3ac2f293cc60a4f9d5898e330a037d9b0a6c9bfd
LLiu34 <62640629+LLiu34@users.noreply.github.com>
2020-03-25 16:30:28 +0800

    Add files via upload

---
+++ buer2.java
@@ -0,0 +1,9 @@
+public class buer2
+{
+            public static void main(String[] args)
+            {
+                 boolean a;
+                 int x=10,y=20,z=30;
+              System.out.println("(x<y+z)&&(x+10<=20) is "+((x<y+z)&&(x+10<=20)));
+           }
+}
