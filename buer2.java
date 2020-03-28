d56cf5f9e3a2477426d7dbf3c301d93b0b1b2ccf
LLiu34 <62640629+LLiu34@users.noreply.github.com>
2020-03-28 16:07:02 +0800

    Delete buer2.java

--- buer2.java
+++
@@ -1,9 +0,0 @@
-public class buer2
-{
-            public static void main(String[] args)
-            {
-                 boolean a;
-                 int x=10,y=20,z=30;
-              System.out.println("(x<y+z)&&(x+10<=20) is "+((x<y+z)&&(x+10<=20)));
-           }
-}
